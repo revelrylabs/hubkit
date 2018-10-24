58d1d620326d4675aa9c1169ca01fb12d325581b
Robert Prehn <3952444+prehnRA@users.noreply.github.com>
2018-10-24 12:52:16 -0500

    chore: Add coveralls support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 require 'simplecov'
+require 'coveralls'
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
 SimpleCov.start
 
 require 'rspec/collection_matchers'
